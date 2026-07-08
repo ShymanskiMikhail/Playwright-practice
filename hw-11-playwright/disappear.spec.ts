a51caeca0d8819e37d104545966414e6847bf505
Mikhail Shymanski <misasimanski@gmail.com>
2026-07-08 14:03:43 +0300

    Added hw11

---
+++ hw-11-playwright/disappear.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/dynamic_controls', 
+    { waitUntil: 'domcontentloaded' });
+  await page.getByRole('button', { name: 'Remove' }).click(); 
+  const result = page.locator('#message');
+  const loader = page.locator('#loading');
+  await expect(loader).toBeVisible();
+  await expect(loader).toBeHidden({ timeout: 20000 });
+  await expect(result).toBeVisible();
+  await expect(result).toHaveText("It's gone!");
+});
