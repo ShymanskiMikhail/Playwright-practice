9ac96d750ecfeca5c044ac5ed21f405c993697cd
Mikhail Shymanski <misasimanski@gmail.com>
2026-07-08 14:18:51 +0300

    auto using prettier onSave

--- hw-11-playwright/disappear.spec.ts
+++ hw-11-playwright/disappear.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/dynamic_controls', 
-    { waitUntil: 'domcontentloaded' });
-  await page.getByRole('button', { name: 'Remove' }).click(); 
+  await page.goto('https://the-internet.herokuapp.com/dynamic_controls', {
+    waitUntil: 'domcontentloaded',
+  });
+  await page.getByRole('button', { name: 'Remove' }).click();
   const result = page.locator('#message');
   const loader = page.locator('#loading');
   await expect(loader).toBeVisible();
--- hw-11-playwright/waits.spec.ts
+++ hw-11-playwright/waits.spec.ts
@@ -4,7 +4,7 @@ test('test', async ({ page }) => {
   await page.goto('http://uitestingplayground.com/ajax', {
     waitUntil: 'domcontentloaded',
   });
-   await page.locator('#ajaxButton').click();
+  await page.locator('#ajaxButton').click();
   const result = page.locator('.bg-success');
   await expect(result).toBeVisible({ timeout: 20000 });
   await expect(result).toHaveText('Data loaded with AJAX get request.');
